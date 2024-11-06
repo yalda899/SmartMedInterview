33b64881c7c7f87ca655e971f30b9bae1863058e
Yalda Khani <y.khani@bluteam.ir>
2024-11-06 19:32:15 +0330

    update test flow and remove 2 test cases

--- test.py
+++ test.py
@@ -3,18 +3,22 @@ import requests
 
 BASE_URL = "https://petstore.swagger.io/v2"
 
-#add a new pet
-def test_add_new_pet():
+def add_new_pet(id , name , status):
     pet_data = {
-        "id": 12345,
-        "name": "Bobby",
-        "status": "available"
+        "id": id,
+        "name":  name,
+        "status":status
     }
     response = requests.post(f"{BASE_URL}/pet", json=pet_data)
+    return response
+
+#add a new pet
+def test_TC_1():
+    response = add_new_pet(id = 12345 , name= "Bobby" , status='available')
     assert response.status_code == 200
     assert response.json()["name"] == "Bobby"
 
-def test_add_new_pet_with_missing_data():
+def test_tc_2():
     pet_data = {
         "id": 12345,
         "status": "available"
@@ -23,7 +27,7 @@ def test_add_new_pet_with_missing_data():
     assert response.status_code == 400 or response.status_code == 422
 
 # update pet status
-def test_update_pet_status():
+def test_TC_3():
     pet_data = {
         "id": 12345,
         "status": "sold"
@@ -32,27 +36,24 @@ def test_update_pet_status():
     assert response.status_code == 200
     assert response.json()["status"] == "sold"
 
-def test_update_non_existent_pet():
-    pet_data = {
-        "id": 99999,
-        "status": "sold"
-    }
-    response = requests.put(f"{BASE_URL}/pet", json=pet_data)
-    assert response.status_code == 404
 
 #delete pet
-def test_delete_pet():
-    pet_id = 12345
+def test_TC_4():
+    response = add_new_pet(id=12345, name="Bobby", status='available')
+    pet_id = response.json()['id']
     response = requests.delete(f"{BASE_URL}/pet/{pet_id}")
     assert response.status_code == 200
 
-def test_delete_non_existent_pet():
-    pet_id = 99999
+def test_TC_5():
+    response = add_new_pet(id=12345, name="Bobby", status='available')
+    pet_id = response.json()['id']
+    response = requests.delete(f"{BASE_URL}/pet/{pet_id}")
+    assert response.status_code == 200
     response = requests.delete(f"{BASE_URL}/pet/{pet_id}")
     assert response.status_code == 404
 
 #place an order
-def test_place_order():
+def test_TC_6():
     order_data = {
         "id": 54321,
         "petId": 12345,
@@ -63,11 +64,3 @@ def test_place_order():
     assert response.status_code == 200
     assert response.json()["status"] == "placed"
 
-def test_place_order_with_missing_data():
-    order_data = {
-        "id": 54321,
-        "quantity": 1,
-        "status": "placed"
-    }
-    response = requests.post(f"{BASE_URL}/store/order", json=order_data)
-    assert response.status_code == 400
